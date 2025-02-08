f5eaebd47b9204662b4a3deb3537c6838708d88b
Juhasen <juszczyk-krystian@wp.pl>
2025-02-08 17:46:49 +0100

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module pl.juhas.kierki {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens pl.juhas.kierki to javafx.fxml;
+    exports pl.juhas.kierki;
+}
